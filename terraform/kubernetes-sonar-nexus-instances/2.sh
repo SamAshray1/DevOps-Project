b48c1c8d4fc7c02882adf6cf2d790c5e63ed9641
SamAshray1 <sam.ashray1@gmail.com>
2025-01-29 20:20:02 +0530

    implemented-sonarqube-nexus

---
+++ terraform/kubernetes-sonar-nexus-instances/2.sh
@@ -0,0 +1,3 @@
+wget https://github.com/Shopify/kubeaudit/releases/tag/v0.22.1
+tar -xvzf https://github.com/Shopify/kubeaudit/releases/download/v0.22.1/kubeaudit_0.22.1_linux_amd64.tar.gz
+sudo mv kubeaudit /usr/local/bin
