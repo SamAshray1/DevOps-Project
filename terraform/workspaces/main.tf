4ae46f4cced4621180872479eb2bd785ca089e34
Sam <sam.ashray1@gmail.com>
2025-02-03 14:41:29 +0000

    added s3 remote backend

--- terraform/workspaces/main.tf
+++ terraform/workspaces/main.tf
@@ -1,4 +1,12 @@
 terraform {
+  backend "s3" {
+    bucket         = "terraform-learning-samuel-devops"
+    key            = "03-basics/web-app/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -8,22 +16,22 @@ terraform {
 }
 
 locals {
- environment_name = terraform.workspace
+  environment_name = terraform.workspace
 }
 
 resource "aws_instance" "example" {
   ami             = "ami-011899242bb902164"
   instance_type   = "t2.micro"
-  key_name        = aws_key_pair.ansible.key_name
+#  key_name        = aws_key_pair.ansible.key_name
   security_groups = [aws_security_group.instances.name]
- 
+
   tags = {
-    Name = "{local.environment_name}-web-server"
+    Name = "${local.environment_name}-web-server"
   }
 
-  provisioner "local-exec" {
-    command = "echo ubuntu@${self.public_dns} > /root/devops-project/ansible/inventory"
-  }
+#  provisioner "local-exec" {
+#    command = "echo ubuntu@${self.public_dns} > /root/Devops-Project/ansible/inventory"
+#  }
 }
 
 resource "aws_security_group" "instances" {
