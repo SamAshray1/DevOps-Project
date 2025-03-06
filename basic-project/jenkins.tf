483ef841823e3e3cf8d10f57b35e35ef429b08a2
Sam <sam.ashray1@gmail.com>
2025-03-06 12:16:30 +0000

    Jenkin startup files

---
+++ basic-project/jenkins.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region = "us-east-1" # Change as needed
+}
+
+resource "aws_key_pair" "jenkins_key" {
+  key_name   = "jenkins-key"
+  public_key = file("jenkins-key.pub")
+}
+
+
+resource "aws_instance" "jenkins" {
+  ami             = "ami-011899242bb902164"
+  instance_type   = "t2.medium"
+  key_name        = aws_key_pair.jenkins_key.key_name
+  security_groups = ["jenkins-sg"]
+  user_data       = file("jenkins-init.sh")
+
+  tags = {
+    Name = "Jenkins-Server"
+  }
+}
+
+resource "aws_security_group" "jenkins_sg" {
+  name        = "jenkins-sg"
+  description = "Allow Jenkins access"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Restrict in production
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Restrict in production
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.jenkins.public_dns
+}
+
